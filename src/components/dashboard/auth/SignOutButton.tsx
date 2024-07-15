f4f521e9a708c31d20bb68b98a7dc74195b51f5b
PAdventures <sdulyea@icloud.com>
2024-07-15 19:48:34 +0100

    Fix: SVG icon failing to render properly

--- src/components/dashboard/auth/SignOutButton.tsx
+++ src/components/dashboard/auth/SignOutButton.tsx
@@ -1,14 +1,14 @@
 "use client"
 
 import { Button } from "@/components/shadcn/ui/button.tsx"
-import Logout from "@mui/icons-material/Logout"
+import { Logout } from "@mui/icons-material"
 import { signOut } from "@/app/dashboard/actions.ts"
 
 export default function SignOutButton() {
     return (
-        <Button className="flex items-center justify-evenly" onClick={async () => await signOut()}>
-            <Logout className=" pr-4 text-[2.5rem]" />
-            <p className="text-sm">Sign Out</p>
+        <Button className="flex items-center justify-around w-[15vh]" onClick={async () => await signOut()}>
+            <Logout />
+            <p className="text-xs">Sign Out</p>
         </Button>
     )
 }
