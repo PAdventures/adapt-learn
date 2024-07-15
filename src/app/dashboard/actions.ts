2f131ffb87b168b4cbffb86637b0c7a6f9c872d5
PAdventures <sdulyea@icloud.com>
2024-07-15 19:50:54 +0100

    Feat: Method to fetch auth and database data for user

--- src/app/dashboard/actions.ts
+++ src/app/dashboard/actions.ts
@@ -1,8 +1,17 @@
 "use server";
 
 import { redirect } from "next/navigation";
-import { createClient } from "../../utils/supabase/server.ts";
+import { createClient } from "@/utils/supabase/server.ts";
 import { revalidatePath } from "next/cache";
+import { DrizzleManager } from "@/classes/DrizzleManagers.ts";
+import { Account } from "~/database/schema/accounts.ts";
+import { User } from "@supabase/supabase-js";
+
+export interface UserDashboardMetadata {
+    data: Account;
+    auth: User;
+    allowSchoolView: boolean;
+}
 
 export async function signOut(): Promise<void> {
     const supabase = createClient();
@@ -15,4 +24,32 @@ export async function signOut(): Promise<void> {
 
     revalidatePath("/", "layout")
     redirect("/")
+}
+
+export async function getUserDashboardMetadata(): Promise<UserDashboardMetadata | undefined> {
+    const supabase = createClient();
+
+    const { error, data } = await supabase.auth.getUser();
+
+    if (error || !data) {
+        return undefined
+    }
+
+    let accountData: Account | undefined;
+
+    try {
+        accountData = await DrizzleManager.accounts.getOneByAuthUid(data.user.id);
+    } catch (error) {
+        return undefined
+    }
+
+    if (!accountData) {
+        return undefined
+    }
+
+    return {
+        data: accountData,
+        auth: data.user,
+        allowSchoolView: ["STUDENT", "TEACHER"].includes(accountData.role)
+    };
 }
