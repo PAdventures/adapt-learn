09f8f288a0f2c6946b8d30dc5dfd6fe77cdb8f84
PAdventures <sdulyea@icloud.com>
2024-07-28 16:36:39 +0100

    Fix + Chore: Fixed SSL signing issue and added dependencies

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,17 @@
-import type { Config } from "drizzle-kit";
+import type { Config } from 'drizzle-kit';
 
-import "dotenv/config"
+import 'dotenv/config';
 
 export default {
-    schema: './database/schema/*.ts',
-    out: './drizzle/migrations/',
-    dialect: "postgresql",
-    dbCredentials: {
-        url: process.env.SUPABASE_CONNECTION_URL,
-        ssl: "require"
-    },
-    verbose: true,
-    strict: true,
-} satisfies Config
+	schema: './database/schema/*.ts',
+	out: './drizzle/migrations/',
+	dialect: 'postgresql',
+	dbCredentials: {
+		url:
+			process.env.SUPABASE_CONNECTION_URL +
+			'?sslmode=verify-full' +
+			'&sslrootcert=database/supabase-root.crt',
+	},
+	verbose: true,
+	strict: true,
+} satisfies Config;
